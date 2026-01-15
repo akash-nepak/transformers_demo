b4bde5649140fd106d995a16a412e3765991c431
akash-nepak <akashnepak7@outlook.com>
2026-01-15 23:36:53 +0530

    change

--- model.py
+++ model.py
@@ -61,19 +61,150 @@ class LayerNormalization(nn.Module): # mean and var are caluclated for each toke
 
      mean = x.mean(dim = -1,keepdim = True)
      std = torch.sqrt(x.var(dim = -1, keepdim = True)+ self.eps)
-     return self.aplha * (x-mean)/ (std) + self.bias
+     return self.alpha * ((x-mean)/ (std)) + self.bias
 
 
-class FeedForwardBlock(nn.Modules):
+class FeedForwardBlock(nn.Module):
 
     def __init__(self,d_model : int, d_ff :int,dropout : float) -> None:
         super().__init__()
 
         self.linear_1 = nn.Linear(d_model,d_ff) #projecting into a higher dimensions 4* d_model
-        self.dropout 
+        self.dropout = nn.Dropout(dropout)
+        self.linear_2 = nn.Linear(d_ff,d_model)
+
+    def forward (self,x):
+        #transforms from (Batch,seq_Len,d_model) --> (BatH, Seq_len,d_ff)
+
+        return self.linear_2(self.dropout(torch.nn.functional.gelu(self.linear_1(x))))
+
+        
+class MultiHeadAttentionBlock(nn.Module):
+
+    def __init__(self,d_model : int,h: int,dropout:float):
+
+        super().__init__() 
+
+        self.d_model = d_model
+        self.h = h
+        assert d_model % h == 0, "d_model is not divisible by h "
+
+        self.d_k = d_model // h 
+
+        self.w_q = nn.Linear(d_model,d_model,)  #creating a learnanble wq,wk,wv matrix
+        self.w_k = nn.Linear(d_model,d_model)
+        self.w_v = nn.Linear(d_model, d_model)
+        self.w_o = nn.Linear(d_model,d_model)
+
+        self.dropout = nn.Dropout(dropout)
+
+
+    @staticmethod
+    def attention(query,key,value,mask,dropout :nn.Dropout):
+
+        d_k = query.shape[-1]
+
+        attention_scores = (query @ key.transpose(-2,-1)) / math.sqrt(d_k)#transposing from (B,H,seq,d_model) --> (B,h,Dmod,seq)
+        if mask is not None:
+
+            attention_scores.masked_fill_(mask ==0, -1e9)
+        attention_scores=attention_scores.softmax(dim = -1) #(Batch, h, S_L,S_l)
+
+        if dropout is not None:
+            attention_scores = dropout(attention_scores)
+
+        return (attention_scores @ value),attention_scores
 
 
 
+        
+
+
+
+
+         
+
+
+    def forward(self,q, k,v,mask): 
+        query = self.w_q(q) #(Batch,Seq_Lenghth,D_model) --> (Batch,Seq_Lenghth,D_model)
+        key = self.w_k(k)  #(Batch,Seq_Lenghth,D_model) --> (Batch,Seq_Lenghth,D_model)
+        value = self.w_v(v) #(Batch,Seq_Lenghth,D_model) --> (Batch,Seq_Lenghth,D_model)
+       
+        query = query.view(query.shape[0],query.shape[1],self.h,self.d_k).transpose (1,2).contiguous()
+
+        #he view command reshapes the tensor. It takes the big embedding vector of size d_model at the end and "cuts" it into h pieces, each of size d_k.
+        #The .transpose(1, 2) - Arranging for Parallel Processing, and seeing full seequence and focusing different aspects of the same sentence 
+        #Transformation: (Batch, Seq_Len, h, d_k) --> (Batch, h, Seq_Len, d_k)
+
+        key = key.view(key.shape[0],key.shape[1],self.h,self.d_k).transpose(1,2)
+        value = value.view(value.shape[0],value.shape[1],self.h,self.d_k).transpose(1,2)
+
+        x,self.attention_scores = MultiHeadAttentionBlock.attention(query,key,value,mask,self.dropout)
+        #(B,H,Seq,dk) --> (B,seq,h,dk) -->(B,S,D_MODEL)
+        x = x.transpose(1,2).contiguous().view(x.shape[0],-1,self.h *self.d_k) 
+
+        return self.w_o(x)
+
+
+class ResidualConnection(nn.Module):
+
+    def __init__(self,dropout:float) -> None:
+        super().__init__()
+
+        self.dropout = nn.Dropout(dropout)
+        self.norm =LayerNormalization()
+
+
+    def forward(self,x,sublayer):
+
+        return x + self.dropout(sublayer(self.norm(x))) #pre normalization for more stability
+
+
+## Building the encoder block ##
+
+
+class EncoderBlock(nn.Module):
+
+    def __init__(self,self_attention_block : MultiHeadAttentionBlock,feed_forward_block : FeedForwardBlock,droput : float) ->None:
+
+        super().__init__()
+
+        self.self_attention_block = self_attention_block
+        self.feed_forward_block = feed_forward_block
+        self.residual_connections = nn.ModuleList([ResidualConnection(droput) for _ in range(2)])
+        #creats 2 instances of the ResidualConnections as needed for encoder 
+
+    def forward(self,x,src_mask):
+
+        x = self.residual_connections[0](x,lambda x: self.self_attention_block(x,x,x,src_mask) )
+        x = self.residual_connections[1](x,self.feed_forward_block) #passes x as first 
+
+
+        return x
+
+        
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
 
 
 
