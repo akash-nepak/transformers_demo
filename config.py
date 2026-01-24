5999fec059deeb935efa697eccf5a7c377601c51
akash-nepak <akashnepak7@outlook.com>
2026-01-24 21:17:56 +0530

    changes

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 def get_config(): #can fully change and play around with parameter
     return {
-        "batch_size" : 8,
+        "batch_size" : 20,
         "num_epochs" : 20,
         "lr": 10**-4,
         "seq_len":350,
